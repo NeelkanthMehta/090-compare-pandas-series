9d568cd0c9a04bd13b46925e5dbc47b36e2d87fb
NeelkanthMehta <neilstrong2003@yahoo.com>
2017-09-20 14:59:55 +0000

    Done

--- build.py
+++ build.py
@@ -5,15 +5,22 @@ def equal_operator(ds1, ds2):
     """
     Enter your code here
     """
-
+    ds3 = ds1 == ds2
+    ds3.tolist()
+    return ds3
 
 def greater_than_operator(ds1, ds2):
     """
     Enter your code here
     """
-
+    ds3 = ds1 > ds2
+    ds3.tolist()
+    return ds3
 
 def less_than_operator(ds1, ds2):
     """
     Enter your code here
     """
+    ds3 = ds1 < ds2
+    ds3.tolist()
+    return ds3
